f528e439d6e85d1ae3128d3c498d392365fc27a9
ARUN <s-rarun@lwsd.org>
2024-02-29 13:46:23 -0800

    More testing/research for UI

--- ui.py
+++ ui.py
@@ -1,31 +1,30 @@
 import pygame
 from math import *
-BLACK = (255, 255, 255)
-WHITE = (0, 0, 0)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
 
-def UI():
+class UI:
     def __init__(self, size):
         self.size = size
         self.tile_size = (self.size / 8)
         self.board = {
-            8: ['', '', '', '', '', '', '', ''],
-            7: ['', '', '', '', '', '', '', ''],
+            8: ['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'],
+            7: ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
             6: ['', '', '', '', '', '', '', ''],
             5: ['', '', '', '', '', '', '', ''],
             4: ['', '', '', '', '', '', '', ''],
             3: ['', '', '', '', '', '', '', ''],
-            2: ['', '', '', '', '', '', '', ''],
-            1: ['', '', '', '', '', '', '', ''],
+            2: ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
+            1: ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'],
         }
         self.screen = pygame.display.set_mode((size, size))
     
-    def draw_grid(self, window):
+    def draw_grid(self):
         for key, value in self.board.items():
-            for square in value:
-                square_index = value.index(square)
-                if (key + square_index) % 2 == 0:
-                    pygame.rect.draw(window, WHITE, (self.tile_size * square_index, self.tile_size * abs(8 - key), self.tile_size, self.tile_size))
+            print(value)
+            for i in range(0, len(value)):
+                if (key + i) % 2 == 0:
+                    pygame.draw.rect(self.screen, WHITE, (self.tile_size * i, self.tile_size * abs(8 - key), self.tile_size, self.tile_size))
                 else:
-                    pygame.rect.draw(window, BLACK, (self.tile_size * square_index, self.tile_size * abs(8 - key), self.tile_size, self.tile_size))
-        
+                    pygame.draw.rect(self.screen, BLACK, (self.tile_size * i, self.tile_size * abs(8 - key), self.tile_size, self.tile_size))
 
