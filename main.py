cf8b53aa04c3c051ef1f924744ac1ce367f96b8d
ARUN <s-rarun@lwsd.org>
2024-03-11 13:30:34 -0700

    Finished adding chess pieces, will check SAN for pgn translation

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from pgn_translator import Translator
 from Movemaker import Movemaker
 pygame.init()
 written_board = chess.Board()
-size = 600
+size = 1000
 square_size = size / 8
 movemaker = Movemaker()
 all_moves = 'e4 e5 Nf3 Nc6 Bb5 g6 O-O Bg7 c3 a6 Ba4 b5 Bc2 Nf6 d4 d6 dxe5 Nxe5 Nxe5 dxe5 Qxd8+ Kxd8 a4 Bb7 f3 Nd7 Be3 h5 Nd2 Bh6 Bxh6 Rxh6 Rfd1 Ke7 Nb3 Rhh8 Na5 Rab8 axb5 axb5 b4 Rhd8 Kf2 Nb6 Rxd8 Kxd8 Bb3 Ke7 Ke3 Ba8 h4 f5 Bc2 f4+ Ke2 Rg8 Bd3 g5 hxg5 Rxg5 Kf2 c6 Nb3 Bb7 Ra7'
@@ -42,6 +42,7 @@ def game_loop():
         print(written_board.move_stack)
         print((written_board))'''
         game_ui.draw_grid()
+        game_ui.draw_pieces()
         pygame.display.update()
         turn += 1
         
--- ui.py
+++ ui.py
@@ -1,12 +1,27 @@
 import pygame
 from math import *
-BLACK = (0, 0, 0)
+BROWN = (50, 50, 50)
 WHITE = (255, 255, 255)
+
+ROOK_W = 'PieceImages/white_rook.png'
+KNIGHT_W = 'PieceImages/white_knight.png'
+BISHOP_W = 'PieceImages/white_bishop.png'
+QUEEN_W = 'PieceImages/white_queen.png'
+KING_W = 'PieceImages/white_king.png'
+PAWN_W = 'PieceImages/white_pawn.png'
+ROOK = 'PieceImages/black_rook.png'
+KNIGHT = 'PieceImages/black_knight.png'
+BISHOP = 'PieceImages/black_bishop.png'
+QUEEN = 'PieceImages/black_queen.png'
+KING = 'PieceImages/black_king.png'
+PAWN = 'PieceImages/black_pawn.png'
+
 
 class UI:
     def __init__(self, size):
         self.size = size
         self.tile_size = (self.size / 8)
+        self.positioner = 0.1
         self.board = {
             8: ['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'],
             7: ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
@@ -18,6 +33,21 @@ class UI:
             1: ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'],
         }
         self.screen = pygame.display.set_mode((size, size))
+        self.piece_size = (int(self.tile_size - self.tile_size * self.positioner), int(self.tile_size - self.tile_size * self.positioner))
+        self.piece_images = {
+            'r': pygame.transform.scale(pygame.image.load(ROOK), self.piece_size), 
+            'n': pygame.transform.scale(pygame.image.load(KNIGHT), self.piece_size), 
+            'b': pygame.transform.scale(pygame.image.load(BISHOP), self.piece_size),
+            'q': pygame.transform.scale(pygame.image.load(QUEEN), self.piece_size), 
+            'k': pygame.transform.scale(pygame.image.load(KING), self.piece_size), 
+            'p': pygame.transform.scale(pygame.image.load(PAWN), self.piece_size),
+            'R': pygame.transform.scale(pygame.image.load(ROOK_W), self.piece_size), 
+            'N': pygame.transform.scale(pygame.image.load(KNIGHT_W), self.piece_size), 
+            'B': pygame.transform.scale(pygame.image.load(BISHOP_W), self.piece_size),
+            'Q': pygame.transform.scale(pygame.image.load(QUEEN_W), self.piece_size), 
+            'K': pygame.transform.scale(pygame.image.load(KING_W), self.piece_size), 
+            'P': pygame.transform.scale(pygame.image.load(PAWN_W), self.piece_size)
+        }
     
     def draw_grid(self):
         for key, value in self.board.items():
@@ -25,5 +55,12 @@ class UI:
                 if (key + i) % 2 == 0:
                     pygame.draw.rect(self.screen, WHITE, (self.tile_size * i, self.tile_size * abs(8 - key), self.tile_size, self.tile_size))
                 else:
-                    pygame.draw.rect(self.screen, BLACK, (self.tile_size * i, self.tile_size * abs(8 - key), self.tile_size, self.tile_size))
+                    pygame.draw.rect(self.screen, BROWN, (self.tile_size * i, self.tile_size * abs(8 - key), self.tile_size, self.tile_size))
 
+    def draw_pieces(self):
+        for key, value in self.board.items():
+            for i in range(0, len(value)):
+                if value[i] != '':
+                    self.screen.blit(self.piece_images[value[i]], 
+                    (i * self.tile_size + self.tile_size * self.positioner / 2, (8 - key) * self.tile_size + self.tile_size * self.positioner / 2))
+                
