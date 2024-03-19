31b5db724eca492f069a5ea3fc02d42f75c0012c
Anivishy <ani.vishy@outlook.com>
2024-03-19 12:32:01 -0700

    Half way through #10

--- chessdotcomapi.py
+++ chessdotcomapi.py
@@ -16,6 +16,8 @@ import concurrent.futures
 from chessdotcom import *
 import webbrowser
 
+import urllib.request
+
 # async def gather_with_concurrency(n, *tasks):
 #     semaphore = asyncio.Semaphore(n)
 #     async def sem_task(task):
@@ -23,6 +25,12 @@ import webbrowser
 #             return await task
    
 #     return await asyncio.gather(*(sem_task(task) for task in tasks))
+
+urls = []
+usernames = ["Hikaru"]
+games = []
+             
+#, "MagnusCarlsen", "nihalsarin", "Firouzja2003", "Polish_fighter3000"]
 
 Client.request_config["headers"]["User-Agent"] = (
     "asgasdf"
@@ -33,13 +41,47 @@ async def post_async(username):
     response = get_player_game_archives(username)
     return response.__getattribute__('json')
 
+async def fetch_async(url):
+    with urllib.request.urlopen(url) as response:
+        games.append(response)
+        pprint(games)
+        return
+        #output = response.decode('utf-8')
+    
 async def main(usernames: list):
     async with aiohttp.ClientSession() as session:
         tasks = [post_async(username) for username in usernames]
-        results = await asyncio.gather(*tasks)
+        for coro in asyncio.as_completed(tasks):
+            #results = await asyncio.gather(*tasks)
+            result = await coro
+            # with open("chessdotcom_urls.txt", 'w') as f:
+            #     f.write(str(result['archives']))
+            urls.append(result['archives'])
 
-    pprint(results)
+async def html_reader(urls):
+    async with aiohttp.ClientSession() as session:
+        urls = urls[-15:] 
+        # ^^^ used to change how far back to go on a users data.
+        #Increasing the value will go back further 1 month in time
+        tasks = [fetch_async(url) for url in urls]
+        for coro in asyncio.as_completed(tasks):
+            result = await coro
+            games.append(result)
+
+    #pprint(results)
 
 if __name__ == "__main__":
-    usernames = ["Hikaru", "MagnusCarlsen", "nihalsarin", "Firouzja2003", "Polish_fighter3000"]
-    asyncio.run(main(usernames))
+    while True:
+        asyncio.run(main(usernames))
+        #pprint(urls)
+        usernames.clear()
+        if len(urls) > 0:
+            for url in urls:
+                print("running")
+                #for line in url:  
+                asyncio.run(html_reader(url))                  
+                for game in games:
+                    #data = urlopen(game)
+                    pprint(game)
+                    pprint(game.read())
+            urls.clear()
