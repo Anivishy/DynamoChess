bf7f1ac92cd77d0ab1bdd9ec694c6be1c81ec2e6
ARUN <s-rarun@lwsd.org>
2024-03-19 13:31:56 -0700

    Simple piece movement, need to fix castling and promotion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import chess
 import pygame
+import time
 from ui import UI
 from pgn_translator import Translator
 from Movemaker import Movemaker
@@ -15,7 +16,16 @@ translator = Translator(all_moves)
 #print(written_board.san(chess.Move.from_uci("g1f3")))
 #print(written_board.parse_san('e4'))
 
-
+def automated_move(turn, moves, game_ui):
+    if turn < len(moves):
+        move = moves[turn]
+        uci = translator.pgn_to_uci(move, written_board)
+        chess_move = chess.Move.from_uci(uci)
+        first_coord, second_coord = translator.uci_to_coordinates(uci)
+        screen_move, promotion, castle_detection = translator.get_move_from_screen(first_coord, second_coord, game_ui.board)
+        if chess_move in written_board.legal_moves:
+            game_ui.selected_piece_movement(second_coord, first_coord, promotion, castle_detection)
+            written_board.push(chess_move)
 
 def game_loop():
     pgn_moves = []
@@ -24,10 +34,11 @@ def game_loop():
     game_over = False
     selected_piece = None
     moves = all_moves.split(' ')
-    for move in moves:
-        uci = translator.pgn_to_uci(move, written_board)
-        print(uci)
-        written_board.push(chess.Move.from_uci(uci))
+    print(translator.uci_to_coordinates('e2e4'))
+    #for move in moves:
+        #uci = translator.pgn_to_uci(move, written_board)
+        #print(uci)
+        #written_board.push(chess.Move.from_uci(uci))
     while not game_over:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -38,22 +49,29 @@ def game_loop():
                     selected_piece = movemaker.get_current_piece_pos(pygame.mouse.get_pos(), game_ui.board, square_size)
                     game_ui.set_selected_square([selected_piece[0], (selected_piece[1])]) # needs to be converted to indices
                     movemaker.change_state()
+                    print(selected_piece)
                 else:
                     new_pos = movemaker.get_current_piece_pos(pygame.mouse.get_pos(), game_ui.board, square_size)
                     screen_move, promotion, castle_detection = translator.get_move_from_screen(game_ui.get_seleceted_square(), new_pos, game_ui.board)
+                    print(screen_move)
                     chess_move = chess.Move.from_uci(screen_move)
                     if chess_move in written_board.legal_moves:
-                        game_ui.selected_piece_movement(new_pos, screen_move, promotion, castle_detection)
+                        game_ui.selected_piece_movement(new_pos, selected_piece, promotion, castle_detection)
                         pgn_moves.append(written_board.san(chess_move))
                         written_board.push(chess_move)
                     game_ui.set_selected_square(None)
                     movemaker.change_state()
                     print(pgn_moves)
-        
-
+        '''
+        if turn < len(moves):
+            move = moves[turn]
+            uci = translator.pgn_to_uci(move, written_board)
+        '''
+        automated_move(turn, moves, game_ui)
         game_ui.draw_grid()
         game_ui.draw_pieces()
         pygame.display.update()
+        time.sleep(1)
         turn += 1
         
 
