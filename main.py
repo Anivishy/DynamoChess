ef42c86220a3fe19e3a5f8b222af205b481e373c
ARUN <s-rarun@lwsd.org>
2024-03-19 12:59:08 -0700

    Converted pgn to uci

--- main.py
+++ main.py
@@ -5,22 +5,29 @@ from pgn_translator import Translator
 from Movemaker import Movemaker
 pygame.init()
 written_board = chess.Board()
-size = 600
+size = 800
 square_size = size / 8
 movemaker = Movemaker()
 all_moves = 'e4 e5 Nf3 Nc6 Bb5 g6 O-O Bg7 c3 a6 Ba4 b5 Bc2 Nf6 d4 d6 dxe5 Nxe5 Nxe5 dxe5 Qxd8+ Kxd8 a4 Bb7 f3 Nd7 Be3 h5 Nd2 Bh6 Bxh6 Rxh6 Rfd1 Ke7 Nb3 Rhh8 Na5 Rab8 axb5 axb5 b4 Rhd8 Kf2 Nb6 Rxd8 Kxd8 Bb3 Ke7 Ke3 Ba8 h4 f5 Bc2 f4+ Ke2 Rg8 Bd3 g5 hxg5 Rxg5 Kf2 c6 Nb3 Bb7 Ra7'
 translator = Translator(all_moves)
 
-print(chess.Move.from_uci("e2e5") in written_board.legal_moves)
-print(written_board.san(chess.Move.from_uci("g1f3")))
-print(written_board.parse_san('e4'))
+#print(chess.Move.from_uci("e2e5") in written_board.legal_moves)
+#print(written_board.san(chess.Move.from_uci("g1f3")))
+#print(written_board.parse_san('e4'))
+
+
 
 def game_loop():
     pgn_moves = []
-    turn = 1
+    turn = 0
     game_ui = UI(size)
     game_over = False
     selected_piece = None
+    moves = all_moves.split(' ')
+    for move in moves:
+        uci = translator.pgn_to_uci(move, written_board)
+        print(uci)
+        written_board.push(chess.Move.from_uci(uci))
     while not game_over:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -42,6 +49,7 @@ def game_loop():
                     game_ui.set_selected_square(None)
                     movemaker.change_state()
                     print(pgn_moves)
+        
 
         game_ui.draw_grid()
         game_ui.draw_pieces()
