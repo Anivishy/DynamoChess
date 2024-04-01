23af1bfac1786f78b1cce0a2a35bebc11b454505
Anivishy <ani.vishy@outlook.com>
2024-04-01 13:21:47 -0700

    Parse url returns and extract & format necessary data #12

--- chessdotcomapi.py
+++ chessdotcomapi.py
@@ -12,11 +12,15 @@ import asyncio
 import aiohttp
 import random
 import concurrent.futures
+import requests
+import xmltojson
 
 from chessdotcom import *
 import webbrowser
 
-import urllib.request
+import urllib.request 
+from urllib.request import urlopen
+import http.client
 
 # async def gather_with_concurrency(n, *tasks):
 #     semaphore = asyncio.Semaphore(n)
@@ -27,9 +31,19 @@ import urllib.request
 #     return await asyncio.gather(*(sem_task(task) for task in tasks))
 
 urls = []
-unchecked_usernames = ["Hikaru", "MagnusCarlsen", "nihalsarin", "Firouzja2003", "Polish_fighter3000"]
+unchecked_usernames = ["Hikaru"] #, "MagnusCarlsen", "nihalsarin", "Firouzja2003", "Polish_fighter3000"]
 checked_usernames = []
 games = []
+active_fetch_threads = []
+parsed_games = {'1000-1399': [],
+                '1400-1799': [],
+                '1800-2199': [],
+                '2200-2599': [],
+                '2600-2999': [],
+                '3000-3399': [],
+                '3400-3799': [],
+                '3800-4199': []
+                }
              
 #, "MagnusCarlsen", "nihalsarin", "Firouzja2003", "Polish_fighter3000"]
 
@@ -72,7 +86,74 @@ async def html_reader(urls):
                 games.append(result)
 
     #pprint(results)
+                
+def get_game_data():
+    print("in get_game_data")
+    threads = [threading.Thread(target= lambda: return_games(https_request)).start() for https_request in games]
 
+    pprint(threads)
+
+
+def return_games(https_request):
+    print("in return_games")
+    #webbrowser.open(https_request.url)
+    game_data = json.loads(urlopen(https_request.url).read())['games'][0:1]
+    #pgns = game_data['games'][0]
+    #pprint(game_data)
+    parse_games_tasking(game_data)
+    #pprint(urllib.request.urlopen(https_request.url))    
+    #pprint(xmltojson.parse(urllib.request.urlopen(https_request.url).read()))
+    #pprint(requests.get(https_request.url))
+    #pprint(data)
+
+def parse_games_tasking(game_data):
+    print("in parse_games_tasking")
+    parse_threads = [threading.Thread(target= lambda: parse_games(game_info)).start() for game_info in game_data]
+
+def parse_games(game_info):
+    #Contains a List of Lists, format of each nested list: [game id, game elo, moves, whiteplayerinfo, blackplayerinfo]
+    pprint(game_info)
+    uuid = game_info['uuid']
+    black = [game_info['black']['@id'], game_info['black']['rating'], game_info['black']['username'], game_info['black']['uuid']]
+    white = [game_info['white']['@id'], game_info['white']['rating'], game_info['white']['username'], game_info['white']['uuid']]
+    #0 = ID, 1 = rating, #2 = username, 3 = uuid
+    if black[2] in checked_usernames:
+        if white[2] not in checked_usernames:
+            unchecked_usernames.append(white[2])
+    else:
+        unchecked_usernames.append(black[2])
+    game_rating = (black[1] + white[1]) // 2
+    moves = game_info['pgn']
+    game_details = [uuid, game_rating, moves, white, black]
+    if game_rating > 1000 and game_rating < 1399:
+        parsed_games['1000-1399'].append(game_details)
+
+    elif game_rating > 1400 and game_rating < 1799:
+        parsed_games['1400-1799'].append(game_details)
+
+    elif game_rating > 1800 and game_rating < 2199:
+        parsed_games['1800-2199'].append(game_details)
+
+    elif game_rating > 2200 and game_rating < 2599:
+        parsed_games['2200-2599'].append(game_details)
+
+    elif game_rating > 2600 and game_rating < 2999:
+        parsed_games['2600-2999'].append(game_details)
+
+    elif game_rating > 3000 and game_rating < 3399:
+        parsed_games['3000-3399'].append(game_details)
+
+    elif game_rating > 3400 and game_rating < 3799:
+        parsed_games['3400-3799'].append(game_details)
+
+    elif game_rating > 3800 and game_rating < 4199:
+        parsed_games['3800-4199'].append(game_details)
+
+    with open("ChessDotComDataBase.txt", 'w') as f:  
+        for k in parsed_games.keys():
+            f.write("'{}':'{}'\n".format(k, parsed_games[k]))
+
+    
 if __name__ == "__main__":
     while True:
         asyncio.run(main(unchecked_usernames))
@@ -85,6 +166,11 @@ if __name__ == "__main__":
                 print("running")
                 #for line in url:  
                 asyncio.run(html_reader(url)) 
+                t1 = threading.Thread(target= get_game_data)
+                t1.start()
+                active_fetch_threads.append(t1)
+                t1.join()
+                print("Loop Complete")
                 #print(len(games))
                 #pprint(games)                 
             urls.clear()
