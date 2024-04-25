3a4c11a10b4ec108f5840c7e0cee80de0d761dd5
Rushil <s-rarun@lwsd.org>
2024-04-25 13:38:01 -0700

    Finished alpha beta pruning 39

--- minimax_algorithm.py
+++ minimax_algorithm.py
@@ -1,6 +1,7 @@
 import chess
 import pandas as pd
 from copy import deepcopy
+import math 
 
 import random
 
@@ -23,33 +24,39 @@ class ChessAI():
         evaluation += material
         #print(evaluation)
         return evaluation
-    def minimax_recursive(self,curBoard,curTurn,curDepth):
+    def minimax_recursive(self,curBoard,curTurn,curDepth, alpha, beta):
         if curDepth == self.max_depth:
             return (self.get_eval_bar(curBoard, curTurn),self.first_move(curBoard.move_stack, self.max_depth)) # TODO: Switch this to evluating all captures
 
         if curTurn == chess.WHITE:
             move_stack = curBoard.move_stack
-            highestEval = (-100000000000,self.first_move(curBoard.move_stack, curDepth))
+            highestEval = (-float(math.inf),self.first_move(curBoard.move_stack, curDepth))
             for i in curBoard.legal_moves:
                 curBoard.push(i)
-                minmaxVal = self.minimax_recursive(curBoard, not curTurn,curDepth+1)
+                minmaxVal = self.minimax_recursive(curBoard, not curTurn,curDepth+1, alpha, beta)
                 if minmaxVal[0]>highestEval[0]:
                     highestEval = minmaxVal
                 curBoard.pop()
+                alpha = max(alpha, highestEval[0])
+                if beta <= alpha:
+                    break
             return highestEval
         else:
             move_stack = curBoard.move_stack
-            lowestEval = (100000000000,self.first_move(curBoard.move_stack, curDepth + 1))
+            lowestEval = (float(math.inf),self.first_move(curBoard.move_stack, curDepth + 1))
             for i in curBoard.legal_moves:
                 curBoard.push(i)
-                minmaxVal = self.minimax_recursive(curBoard,not curTurn,curDepth+1)
+                minmaxVal = self.minimax_recursive(curBoard,not curTurn,curDepth+1, alpha, beta)
                 if minmaxVal[0]<lowestEval[0]:
                     lowestEval = minmaxVal
                 curBoard.pop()
+                beta = min(beta, lowestEval[0])
+                if beta <= alpha:
+                    break
             return lowestEval
         
     def get_ai_move(self,board,turn):
-        best_outcome = self.minimax_recursive(board,turn,0)
+        best_outcome = self.minimax_recursive(board,turn,0, float(-math.inf), float(math.inf))
         print(best_outcome[0])
         return best_outcome[1]
     
