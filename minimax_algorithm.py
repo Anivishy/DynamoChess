b057ab465fe30d7dc4cd687dce7d733fa56c55a1
Anivishy <ani.vishy@outlook.com>
2024-05-06 12:44:07 -0700

    Async Threading, still need to test DO NOT MERGE TO MAIN Threading to increase efficiency of code for board searches #48

--- minimax_algorithm.py
+++ minimax_algorithm.py
@@ -7,6 +7,9 @@ from pgn_translator import Translator
 from threading import Thread
 import threading
 import random
+import asyncio
+import time
+
 
 class ThreadWithReturnValue(Thread):
     
@@ -52,52 +55,109 @@ class ChessAI():
         #print(material * 2, num_legal_moves * 0.02, center_control_heuristic * 0.075)
         
         return evaluation
-    def minimax_recursive(self,curBoard,curTurn,curDepth, alpha, beta, move_object_moves):
+    async def recursive_async_helper_white (self,curBoard,curTurn,curDepth, alpha, beta, move_object_moves):
+        #####################
+        # ORIGINAL VAL CALC #
+        #####################
+        minmaxVal = self.minimax_recursive(curBoard, not curTurn,curDepth+1, alpha, beta, move_object_moves)
+        #################
+        if minmaxVal[0]>highestEval[0]:
+            highestEval = minmaxVal
+        #curBoard.pop()
+        alpha = max(alpha, highestEval[0])
+        if beta <= alpha:
+            pass
+
+    async def recursive_async_helper_black (self,curBoard,curTurn,curDepth, alpha, beta, move_object_moves):
+        #####################
+        # ORIGINAL VAL CALC #
+        #####################
+        minmaxVal = self.minimax_recursive(curBoard,not curTurn,curDepth+1, alpha, beta, move_object_moves)
+        if minmaxVal[0]<lowestEval[0]:
+            lowestEval = minmaxVal
+        #curBoard.pop()
+        beta = min(beta, lowestEval[0])
+        if beta <= alpha:
+            pass
+        
+
+    async def minimax_recursive(self,curBoard,curTurn,curDepth, alpha, beta, move_object_moves):
         if curDepth == self.max_depth:
             return (self.get_eval_bar(curBoard, curTurn, move_object_moves),self.first_move(curBoard.move_stack, self.max_depth)) # TODO: Switch this to evluating all captures
         move_object_moves = self.center_contol.legal_move_manipulation(curBoard)
         if curTurn == chess.WHITE:
             move_stack = curBoard.move_stack
             highestEval = (-float(math.inf),self.first_move(curBoard.move_stack, curDepth))
-            for i in curBoard.legal_moves:
-                curBoard.push(i)
-                #################
-                white_minmax_thread = ThreadWithReturnValue(target = self.minimax_recursive, 
-                                                            args = [curBoard, not curTurn, curDepth + 1, alpha, beta, move_object_moves])
-                white_minmax_thread.start()
-                self.white_minmax.append(white_minmax_thread)
-                minmaxVal = white_minmax_thread.join()
-                #minmaxVal = self.minimax_recursive(curBoard, not curTurn,curDepth+1, alpha, beta, move_object_moves)
-                if minmaxVal[0]>highestEval[0]:
-                    highestEval = minmaxVal
+            tasks = []
+            for move in curBoard.legal_moves:
+                curBoard.push(move)
+                tasks.append(self.recursive_async_helper_white(curBoard,curTurn,curDepth, alpha, beta, move_object_moves))
                 curBoard.pop()
-                alpha = max(alpha, highestEval[0])
-                if beta <= alpha:
-                    break
+            # for coro in asyncio.as_completed(tasks):
+            #     result = await coro
+            
+            # for i in curBoard.legal_moves:
+            #     curBoard.push(i)
+            #     ######################
+            #     # THREADING VAL CALC #
+            #     ######################
+            #     # white_minmax_thread = ThreadWithReturnValue(target = self.minimax_recursive, 
+            #     #                                             args = [curBoard, not curTurn, curDepth + 1, alpha, beta, move_object_moves])
+            #     # white_minmax_thread.start()
+            #     # self.white_minmax.append(white_minmax_thread)
+            #     # minmaxVal = white_minmax_thread.join()
+
+            #     #####################
+            #     # ORIGINAL VAL CALC #
+            #     #####################
+            #     #minmaxVal = self.minimax_recursive(curBoard, not curTurn,curDepth+1, alpha, beta, move_object_moves)
+                
+            #     if minmaxVal[0]>highestEval[0]:
+            #         highestEval = minmaxVal
+            #     curBoard.pop()
+            #     alpha = max(alpha, highestEval[0])
+            #     if beta <= alpha:
+            #         break
+            print(curBoard)
             return highestEval
         else:
             move_stack = curBoard.move_stack
             lowestEval = (float(math.inf),self.first_move(curBoard.move_stack, curDepth + 1))
+            tasks = []
             for i in curBoard.legal_moves:
                 curBoard.push(i)
-                ################
-                black_minmax_thread = ThreadWithReturnValue(target = self.minimax_recursive, 
-                                                            args = [curBoard, not curTurn, curDepth + 1, alpha, beta, move_object_moves])
-                black_minmax_thread.start()
-                self.black_minmax.append(black_minmax_thread)
-                minmaxVal = black_minmax_thread.join()
-                #minmaxVal = self.minimax_recursive(curBoard,not curTurn,curDepth+1, alpha, beta, move_object_moves)
-                if minmaxVal[0]<lowestEval[0]:
-                    lowestEval = minmaxVal
+                tasks.append(self.recursive_async_helper_white(curBoard,curTurn,curDepth, alpha, beta, move_object_moves))
                 curBoard.pop()
-                beta = min(beta, lowestEval[0])
-                if beta <= alpha:
-                    break
+                #time.sleep(1)
+            # for i in curBoard.legal_moves:
+            #     curBoard.push(i)
+            #     ######################
+            #     # THREADING VAL CALC #
+            #     ######################
+            #     black_minmax_thread = ThreadWithReturnValue(target = self.minimax_recursive, 
+            #                                                 args = [curBoard, not curTurn, curDepth + 1, alpha, beta, move_object_moves])
+            #     black_minmax_thread.start()
+            #     self.black_minmax.append(black_minmax_thread)
+            #     minmaxVal = black_minmax_thread.join()
+
+            #     #####################
+            #     # ORIGINAL VAL CALC #
+            #     #####################
+            #     #minmaxVal = self.minimax_recursive(curBoard,not curTurn,curDepth+1, alpha, beta, move_object_moves)
+            #     if minmaxVal[0]<lowestEval[0]:
+            #         lowestEval = minmaxVal
+            #     curBoard.pop()
+            #     beta = min(beta, lowestEval[0])
+            #     if beta <= alpha:
+            #         break
+            print(curBoard)
             return lowestEval
+        
+    
         
     def get_ai_move(self,board,turn):
         move_object_moves = self.center_contol.legal_move_manipulation(board)
-        best_outcome = self.minimax_recursive(board,turn,0, float(-math.inf), float(math.inf), move_object_moves)
+        best_outcome = asyncio.run(self.minimax_recursive(board,turn,0, float(-math.inf), float(math.inf), move_object_moves))
         print(best_outcome[0])
         #print(turn)
         return best_outcome[1]
