7d1744c22beee7a90f69d2f7dc739207c90796f9
Rushil <s-rarun@lwsd.org>
2024-05-09 13:46:00 -0700

    Working on quicksort for move ordering #55

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ def game_loop():
     pgn_moves = []
     turn = 0
     game_ui = UI(size)
-    ai = ChessAI(3, translator, heuristic)
+    ai = ChessAI(5, translator, heuristic)
     game_over = False
     selected_piece = None
     ai_move = False # change this to true to make computer play as white
