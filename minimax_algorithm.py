5563577987cc9936bc417ab7c5916ea006d3258d
Rushil <s-rarun@lwsd.org>
2024-05-23 13:12:14 -0700

    Testing Center Control, will branch off now

--- minimax_algorithm.py
+++ minimax_algorithm.py
@@ -34,6 +34,7 @@ class ChessAI():
         self.black_minmax = []
         self.positions = 0
         self.positions_reg_search = 0
+        self.pruning = 0
 
 
     def sort_partition(self, moves_scores_list, low, high):
@@ -59,23 +60,23 @@ class ChessAI():
         evaluation = 0
         material = self.heuristic.piece_values(board)
         num_legal_moves = board.legal_moves.count()
-        #center_control_heuristic = self.heuristic.get_center_control_value(board, self.center_contol, move_object_moves)
+        center_control_heuristic = self.heuristic.get_center_control_value(board, self.center_contol, move_object_moves)
         #print(center_control_heuristic)
         if curTurn: # white
             
             #print("White")
             # TODO: Need to look for checkmate here. If the last move is mate we need detection. 
             evaluation += num_legal_moves * 0.01
-            #evaluation += center_control_heuristic * 0.09
+            evaluation += center_control_heuristic * 0.01
         else:
             
             #print("Black")
             # TODO: Need to look for checkmate here.
             evaluation += -num_legal_moves * 0.01
-            #evaluation += -center_control_heuristic * 0.09
+            evaluation += -center_control_heuristic * 0.01
         evaluation += material * 2
         #print(material * 2, num_legal_moves * 0.02, center_control_heuristic * 0.075)
-        return evaluation, (material * 2, num_legal_moves * 0.01, '''center_control_heuristic * 0.03, center_control_heuristic''', curTurn, deepcopy(board))
+        return evaluation, (material * 2, num_legal_moves * 0.01, center_control_heuristic * 0.03, center_control_heuristic, curTurn, deepcopy(board))
 
     def captures_only_search(self, curBoard, curTurn, depth, alpha, beta, move_object_moves):
         capture_legal_moves = self.heuristic.legal_move_manipulation(curBoard, self.translator.uci_to_coordinates)[1]
@@ -99,6 +100,7 @@ class ChessAI():
                 curBoard.pop()
                 alpha = max(alpha, highestEval[0][0])
                 if beta <= alpha:
+                    self.pruning += 1
                     break
             return highestEval
         else:
@@ -113,6 +115,7 @@ class ChessAI():
                 curBoard.pop()
                 beta = min(beta, lowestEval[0][0])
                 if beta <= alpha:
+                    self.pruning += 1
                     break
             return lowestEval
 
@@ -143,6 +146,7 @@ class ChessAI():
                 curBoard.pop()
                 alpha = max(alpha, highestEval[0][0])
                 if beta <= alpha:
+                    self.pruning += 1
                     break
             return highestEval
         else:
@@ -164,17 +168,20 @@ class ChessAI():
                 curBoard.pop()
                 beta = min(beta, lowestEval[0][0])
                 if beta <= alpha:
+                    self.pruning += 1
                     break
             return lowestEval
         
     def get_ai_move(self,board,turn):
         self.positions = 0
         self.positions_reg_search = 0
+        self.pruning = 0
         move_object_moves = self.center_contol.legal_move_manipulation(board)
         best_outcome = self.minimax_recursive(board,turn,0, float(-math.inf), float(math.inf), move_object_moves)
         print(best_outcome[0])
         print(best_outcome[0][1][-1])
         print(self.positions, self.positions_reg_search)
+        print(self.pruning)
         return best_outcome[1]
     
     def first_move(self, move_stack, depth):
